770aed5924fc3a2ece6538caaedbf91a6126f7b2
Atul <magnetar1034@gmail.com>
2026-06-13 00:38:56 +0530

    day 1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Atul Bhaskar",
+    author_email="magnetar1034@gmail.com",
+    packages=find_packages()
+)
