80a3f1062ca8beb9ed17ff7a57ec6bef78ff34db
Atul <magnetar1034@gmail.com>
2026-06-14 01:37:35 +0530

    model trainer added

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
+import numpy as np
+# Monkey patch numpy to restore the removed float_ alias
+np.float_ = np.float64 
+
 from us_visa.pipline.training_pipeline import TrainPipeline
 
 obj = TrainPipeline()
